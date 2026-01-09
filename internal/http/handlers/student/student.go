e3ab7c97f0da87753f8a49d54d6e488dd359937d
Manish Chaudhary <cmanish049@gmail.com>
2026-01-09 14:35:51 +0545

    add validations

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -1,9 +1,42 @@
 package student
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
+
+	"github.com/cmanish049/students-api/internal/types"
+	"github.com/cmanish049/students-api/internal/utils/response"
+	"github.com/go-playground/validator/v10"
+)
 
 func New() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome to students api"))
+		slog.Info("create a student")
+
+		var student types.Student
+		err := json.NewDecoder(r.Body).Decode(&student)
+
+		if errors.Is(err, io.EOF) {
+			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(fmt.Errorf("empty body")))
+			return
+		}
+
+		if err != nil {
+			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(err))
+			return
+		}
+
+		// request validation
+		if err = validator.New().Struct(student); err != nil {
+			validateErrs := err.(validator.ValidationErrors)
+			response.WriteJson(w, http.StatusBadRequest, response.ValidationError(validateErrs))
+			return
+		}
+
+		response.WriteJson(w, http.StatusCreated, map[string]string{"success": "OK"})
 	}
 }
